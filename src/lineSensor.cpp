f559137447f1eb49c9d48042c630704a11eec59d
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2023-02-23 16:25:43 +0000

    Added Line Sensing functionality

--- src/lineSensor.cpp
+++ src/lineSensor.cpp
@@ -12,8 +12,27 @@ void LineSensor::lineSensorCalibration() {
     Serial1.println("Calibration Complete");
 }
 
-void LineSensor::lineSensorTest() {
-    int value = lineSensor.readLine(lineSensorValues);
-    Serial1.println("line is:");
-    Serial1.println(value);
+void LineSensor::lineSensorRead() {
+    lineSensor.readCalibrated(lineSensorValues);
+    left1 = lineSensorValues[0];
+    left2 = lineSensorValues[1];
+    right1 = lineSensorValues[4];
+    right2 = lineSensorValues[5];
+    //pos = lineSensor.readLine(lineSensorValues);
+}
+
+
+unsigned int LineSensor::leftValue1() {
+    return left1;
+}
+unsigned int LineSensor::leftValue2() {
+    return left2;
+}
+
+unsigned int LineSensor::rightValue1() {
+    return right1;
+}
+
+unsigned int LineSensor::rightValue2() {
+    return right2;
 }
