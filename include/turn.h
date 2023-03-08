28da9d695c23f5bfd81458a06af9fbe2dbcbe446
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2023-03-08 15:05:47 +0000

    Added Pragma

--- include/turn.h
+++ include/turn.h
@@ -3,6 +3,10 @@
 #include <Zumo32U4.h>
 #include "gyroSensor.h"
 
+// header file for the turning adds the appropriate libraries and files
+// Sets the motor variable object and speeds as well as the angle for the gyros
+// Creates a gyro object
+// Various functions covered in the cpp file
 class Turn
 {
 private:
